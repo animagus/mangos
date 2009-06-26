6334d35baca39006ce3cc1d765cbd4461e970a0d
SeT <can15@ngs.ru>
2009-06-26 22:49:56 +0700

    fix fix

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5387,8 +5387,8 @@ bool Spell::CheckTarget( Unit* target, uint32 eff )
         // in case TARGET_SCRIPT target selected by server always and can't be cheated
         if( (target->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE) &&
             m_spellInfo->EffectImplicitTargetA[eff] != TARGET_SCRIPT &&
-            m_spellInfo->EffectImplicitTargetB[eff] != TARGET_SCRIPT) ||
-			(m_spellInfo->SpellIconID == 45 &&
+            m_spellInfo->EffectImplicitTargetB[eff] != TARGET_SCRIPT) &&
+			!(m_spellInfo->SpellIconID == 45 &&
 			m_spellInfo->EffectImplicitTargetA[eff] == TARGET_EFFECT_SELECT))
             return false;
     }
