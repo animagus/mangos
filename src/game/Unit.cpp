44e6315c0c0af638d79c3ddd20f729df5bc94f64
SeT <can15@ngs.ru>
2009-07-21 17:01:54 +0700

    fix typo

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5523,7 +5523,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                     Unit *caster = triggeredByAura->GetCaster();
                     if (!caster)
                         return false;
-                    pVictim->pVictim->CastSpell(pVictim, 20267,true, triggeredByAura);
+                    pVictim->CastSpell(pVictim, 20267,true, triggeredByAura);
                     return true;
                 }
                 // Judgement of Wisdom
