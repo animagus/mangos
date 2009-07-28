028c03f0aca7cf94600ea014344b8f7a457dacc7
SeT <can15@ngs.ru>
2009-07-28 14:22:10 +0700

    * Refix KJ

--- src/bindings/ScriptDev2/scripts/zone/sunwell_plateau/boss_kiljaeden.cpp
+++ src/bindings/ScriptDev2/scripts/zone/sunwell_plateau/boss_kiljaeden.cpp
@@ -680,7 +680,8 @@ struct MANGOS_DLL_DECL boss_kiljaedenAI : public Scripted_NoMovementAI
         // Reset the controller
         if(m_pInstance)
             m_pInstance->SetData(TYPE_KILJAEDEN, NOT_STARTED);
-		m_creature->setDeathState(JUST_DIED);
+		//m_creature->setDeathState(JUST_DIED);
+		((TemporarySummon *)m_creature)->UnSummon();
     }
 
     void Aggro(Unit* who)
