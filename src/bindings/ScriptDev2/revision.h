864aafafcab97fa0d20c5d945b8879be19780920
SeT <can15@ngs.ru>
2009-08-15 02:27:27 +0700

    * Add file

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1266"
- #define REVISION_DATE "2009-07-26"
- #define REVISION_TIME "13:10:25"
+ #define REVISION_ID "1275"
+ #define REVISION_DATE "2009-07-29"
+ #define REVISION_TIME "13:34:55"
 #endif // __REVISION_H__
