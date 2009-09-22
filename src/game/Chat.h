034f2fc953aa808212aecab680af10c48ec5b6e3
Andrey Rublev <version@.(none)>
2009-09-23 00:47:59 +0700

    [fix]: add HandleMoodCommand to Chat.h
    Signed-off-by: Andrey Rublev <version@wowacadem.ru>

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -429,6 +429,7 @@ class ChatHandler
 
         bool HandleHelpCommand(const char* args);
         bool HandleCommandsCommand(const char* args);
+        bool HandleMoodCommand(const char* args);
         bool HandleStartCommand(const char* args);
         bool HandleDismountCommand(const char* args);
         bool HandleSaveCommand(const char* args);
