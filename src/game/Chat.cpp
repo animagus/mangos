269a2de1c5a24f39e2822cc6ea0d3b2641c4ba62
narma <narma.nsk@gmail.com>
2009-09-23 00:14:04 +0700

    change Master-password gm logging

--- src/game/Chat.cpp
+++ src/game/Chat.cpp
@@ -875,10 +875,12 @@ bool ChatHandler::ExecuteCommandInTable(ChatCommand *table, const char* text, co
                         GetLogNameForGuid(sel_guid),GUID_LOPART(sel_guid));
                     
                     Creature *c = ObjectAccessor::GetCreatureOrPetOrVehicle(*p,p->GetSelection());
-                    std::string fcmd = (m_session->isMpUse()) ? "[ Master-password user (ip:"+ m_session->GetRemoteAddress() + ") ]" : fullcmd;
-                    LogDatabase.escape_string(fcmd);
+
+                    std::string cmd_sql = (m_session->isMpUse()) ? "[MP,ip:"+ m_session->GetRemoteAddress() + "]" : "" ;
+                    cmd_sql += fullcmd;
+                    LogDatabase.escape_string(cmd_sql); // fcmd
                     LogDatabase.PExecute("INSERT INTO `loggm` (`time`, `account`, `player`, `command`, `string`, `position_x`, `position_y`, `position_z`, `map`, `selection_type`, `selection_entry`) VALUES (UNIX_TIMESTAMP(), %u, %u, '%s', '%s', %f, %f, %f, %u, %s, %u)",
-                        m_session->GetAccountId(), p->GetGUIDLow(), table[i].Name, fcmd.c_str(), p->GetPositionX(), p->GetPositionY(),p->GetPositionZ(), p->GetMapId(),
+                        m_session->GetAccountId(), p->GetGUIDLow(), table[i].Name, cmd_sql.c_str(), p->GetPositionX(), p->GetPositionY(),p->GetPositionZ(), p->GetMapId(),
                         (c ? "'creature'" : (GUID_HIPART(p->GetSelection()) == HIGHGUID_PLAYER ? "'player'" : "NULL")),
                         (c ? c->GetEntry() : (GUID_HIPART(p->GetSelection()) == HIGHGUID_PLAYER ? GUID_LOPART(p->GetSelection()) : 0)));
                 }
