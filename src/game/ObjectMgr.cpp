12308917e849acdf1350c13059f1f52fa932aba7
selector <limurchick@gmail.com>
2010-02-09 14:39:11 +0200

    anicheat cwn

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -5023,7 +5023,7 @@ void ObjectMgr::LoadAreaTriggerScripts()
     sLog.outString( ">> Loaded %u areatrigger scripts", count );
 }
 //use searched_node for search some known node
-uint32 ObjectMgr::GetNearestTaxiNode( float x, float y, float z, uint32 mapid, uint32 team )
+uint32 ObjectMgr::GetNearestTaxiNode( float x, float y, float z, uint32 mapid, uint32 team, uint32 searched_node )
 {
     bool found = false;
     float dist;
@@ -5033,8 +5033,21 @@ uint32 ObjectMgr::GetNearestTaxiNode( float x, float y, float z, uint32 mapid, u
     {
         TaxiNodesEntry const* node = sTaxiNodesStore.LookupEntry(i);
 
-        if(!node || node->map_id != mapid || !node->MountCreatureID[team == ALLIANCE ? 1 : 0])
+        if (!node || node->map_id != mapid)
             continue;
+
+        float dist2 = (node->x - x)*(node->x - x)+(node->y - y)*(node->y - y)+(node->z - z)*(node->z - z);
+
+        if (searched_node != 0 && i == searched_node){
+            id = i;
+            dist = dist2;
+            break;
+        }
+
+        if(!node->MountCreatureID[team == ALLIANCE ? 1 : 0])
+            continue;
+
+       // float dist2 = (node->x - x)*(node->x - x)+(node->y - y)*(node->y - y)+(node->z - z)*(node->z - z);
 
         uint8  field   = (uint8)((i - 1) / 32);
         uint32 submask = 1<<((i-1)%32);
@@ -5043,7 +5056,6 @@ uint32 ObjectMgr::GetNearestTaxiNode( float x, float y, float z, uint32 mapid, u
         if((sTaxiNodesMask[field] & submask)==0)
             continue;
 
-        float dist2 = (node->x - x)*(node->x - x)+(node->y - y)*(node->y - y)+(node->z - z)*(node->z - z);
         if(found)
         {
             if(dist2 < dist)
@@ -5059,6 +5071,11 @@ uint32 ObjectMgr::GetNearestTaxiNode( float x, float y, float z, uint32 mapid, u
             id = i;
         }
     }
+
+    //movement anticheat fix
+    if (dist > 3600)
+       id = 0;
+    //movement anticheat fix
 
     return id;
 }
