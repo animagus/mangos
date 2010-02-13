cd16078a26a9793a5173f2b95e18b738260bd706
reagent <kirill.vyazigin@gmail.com>
2010-02-13 15:16:16 +0600

    * Fixed stack effects from spells 31801 and 53736 with 53380 and ranks.

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1804,6 +1804,11 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
                 // Seal of Corruption (caster/target parts stacking allow, other stacking checked by spell specs)
                 if (spellInfo_1->SpellIconID == 2292 && spellInfo_2->SpellIconID == 2292)
                     return false;
+
+                // Seal of Vengeance/Corruption and Righteous Vengeance
+                if (spellInfo_1->SpellIconID == 2292 && spellInfo_2->SpellIconID == 3025 ||
+                    spellInfo_2->SpellIconID == 2292 && spellInfo_1->SpellIconID == 3025)
+                    return false;
             }
 
             // Blessing of Sanctuary (multi-family check, some from 16 spell icon spells)
