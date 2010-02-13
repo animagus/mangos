a59c4c8265dec8577983682cd8b09e668f586440
reagent <kirill.vyazigin@gmail.com>
2010-02-14 02:14:50 +0600

    * Changed triggered spell in talent 51523 and ranks according to the patch 3.2.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6674,7 +6674,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 // Earthen Power (Rank 1,2)
                 case 51523:
                 case 51524:
-                    triggered_spell_id = 63532;
+                    triggered_spell_id = 59566;
                     break;
                 // Shaman T8 Elemental 4P Bonus
                 case 64928:
