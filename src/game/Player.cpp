b41a0109db379f120e4bdc9894fada44955c838f
Feanordev <feanordev@gmail.com>
2010-02-24 22:12:29 +0100

    + Refund system for items purchased with an Alternate Currency, problem left; refund info cant be loaded after server crash due extendedcost being stored in vendor info not item itself

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18355,6 +18355,9 @@ bool Player::BuyItemFromVendor(uint64 vendorguid, uint32 item, uint8 count, uint
             data << uint32(count);
             GetSession()->SendPacket(&data);
 
+            if( it->IsEligibleForRefund() && crItem->ExtendedCost != NULL )
+                AddRefundable(it->GetGUID(), crItem->ExtendedCost);
+
             SendNewItem(it, pProto->BuyCount*count, true, false, false);
         }
     }
@@ -18399,6 +18402,9 @@ bool Player::BuyItemFromVendor(uint64 vendorguid, uint32 item, uint8 count, uint
             data << uint32(crItem->maxcount > 0 ? new_count : 0xFFFFFFFF);
             data << uint32(count);
             GetSession()->SendPacket(&data);
+
+            if( it->IsEligibleForRefund() && crItem->ExtendedCost != NULL )
+                AddRefundable(it->GetGUID(), crItem->ExtendedCost);
 
             SendNewItem(it, pProto->BuyCount*count, true, false, false);
 
@@ -22047,3 +22053,42 @@ void Player::SetHomebindToCurrentPos()
         m_homebindMapId, m_homebindZoneId, m_homebindX, m_homebindY, m_homebindZ, GetGUIDLow());
 }
 
+void Player::CompletedAchievement(AchievementEntry const* entry)
+{
+    GetAchievementMgr().CompletedAchievement(entry);
+}
+
+void Player::AddRefundable( uint64 itemGUID,  uint32 extendedcost )
+{
+    std::pair< uint64, uint32 > insertpair;
+    
+    Item *item = GetItemByGuid(itemGUID);
+
+    if( item == NULL )
+        return;
+	
+    item->SetPlayedtimeField(GetTotalPlayedTime());
+
+    insertpair.first = itemGUID;
+    insertpair.second = extendedcost;
+
+    sObjectMgr.mItemRefundableMap.insert(insertpair);
+}
+
+void Player::RemoveRefundable(uint64 itemGUID)
+{
+    sObjectMgr.mItemRefundableMap.erase(itemGUID);
+}
+
+uint32 Player::LookupRefundable(uint64 itemGUID)
+{
+    ItemRefundableMap::iterator itr;
+    uint32 RefundableEntry = 0;
+
+    itr = sObjectMgr.mItemRefundableMap.find(itemGUID);
+
+    if (itr != sObjectMgr.mItemRefundableMap.end())
+        RefundableEntry = itr->second;
+
+    return RefundableEntry;
+}
