c2463017df43f5d8396a475b2c1dbf37e24f8852
Andrey Rublev <version@wowacadem.ru>
2010-03-24 13:11:05 +0600

    cherry-pick b5a46f1d3ef0bd6e613565cc67cbae508367f1d3

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7149,7 +7149,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 const PlayerSpellMap& sp_list = ((Player*)this)->GetSpellMap();
                 for (PlayerSpellMap::const_iterator itr = sp_list.begin(); itr != sp_list.end(); ++itr)
                 {
-                    if(!itr->second.active || itr->second.disabled || itr->second.state == PLAYERSPELL_REMOVED)
+                    if(!itr->second->active || itr->second->disabled || itr->second->state == PLAYERSPELL_REMOVED)
                         continue;
 
                     SpellEntry const *spellInfo = sSpellStore.LookupEntry(itr->first);
