26c9709dd6e2009b7b4e6623a4abd2da4f5160ff
SeT <set@wowacadem.ru>
2010-05-12 21:16:59 +0700

    Fixed 19804 Arcanite Dragonling

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4395,6 +4395,9 @@ void Spell::EffectSummonGuardian(uint32 i, uint32 forceFaction)
 
     int32 amount = damage > 0 ? damage : 1;
 
+    if (m_spellInfo->Id == 19804)
+        amount = 1;
+
     for(int32 count = 0; count < amount; ++count)
     {
         Pet* spawnCreature = new Pet(GUARDIAN_PET);
