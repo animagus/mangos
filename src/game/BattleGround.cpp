24d6780b82d06206a7781382c01af97e6f197df4
KillerFrca <KillerFrca@gethub.com>
2010-07-05 02:15:20 +0600

    [206] Include honor from kills in battleground stats

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -1402,11 +1402,7 @@ void BattleGround::UpdatePlayerScore(Player *Source, uint32 type, uint32 value)
         case SCORE_BONUS_HONOR:                             // Honor bonus
             // do not add honor in arenas
             if (isBattleGround())
-            {
-                // reward honor instantly
-                if (Source->RewardHonor(NULL, 1, (float)value))
-                    itr->second->BonusHonor += value;
-            }
+                itr->second->BonusHonor += value;
             break;
             //used only in EY, but in MSG_PVP_LOG_DATA opcode
         case SCORE_DAMAGE_DONE:                             // Damage Done
