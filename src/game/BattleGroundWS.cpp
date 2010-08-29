fd4a2e65710f236877242e3dc8ff2f1ec5b37bd0
mosst <Makcim_Chaos@Ukr.net>
2010-08-29 19:59:47 +0300

    [bws213] support Focused and Brutal Assault in WSG.

--- src/game/BattleGroundWS.cpp
+++ src/game/BattleGroundWS.cpp
@@ -86,6 +86,35 @@ void BattleGroundWS::Update(uint32 diff)
                 RespawnFlagAfterDrop(HORDE);
             }
         }
+        
+        if (m_FlagState[BG_TEAM_ALLIANCE] == BG_WS_FLAG_STATE_ON_PLAYER && m_FlagState[BG_TEAM_HORDE] == BG_WS_FLAG_STATE_ON_PLAYER)
+        {
+            if (m_AssaultTimer < BG_WS_FOCUSED_ASSAULT)
+            {
+                uint32 spellAssault;
+                if (m_AssaultTimer < diff)
+                    spellAssault = BG_WS_SPELL_BRUTAL_ASSAULT;
+                else 
+                {
+                    spellAssault = BG_WS_SPELL_FOCUSED_ASSAULT;
+                    m_AssaultTimer -= diff;
+                }
+
+                for(uint8 i = 0; i < BG_TEAMS_COUNT; i++)
+                {
+                    Player* carrier = sObjectMgr.GetPlayer(m_FlagKeepers[i]);
+                    if(!carrier)
+                        continue;
+
+                    if(!carrier->HasAura(spellAssault))
+                        carrier->CastSpell(carrier, spellAssault, true);
+                }
+            }
+            else
+                m_AssaultTimer -= diff;
+        }
+        else
+            m_AssaultTimer = BG_WS_BRUTAL_ASSAULT;
 
         if (m_EndTimer <= diff)
         {
@@ -324,6 +353,19 @@ void BattleGroundWS::EventPlayerDroppedFlag(Player *Source)
 
     if (set)
     {
+        if (Source->HasAura(BG_WS_SPELL_BRUTAL_ASSAULT))
+            Source->RemoveAurasDueToSpell(BG_WS_SPELL_BRUTAL_ASSAULT);
+        else if (Source->HasAura(BG_WS_SPELL_FOCUSED_ASSAULT))
+            Source->RemoveAurasDueToSpell(BG_WS_SPELL_FOCUSED_ASSAULT);
+
+        if (Player* carrier = sObjectMgr.GetPlayer(m_FlagKeepers[(Source->GetTeam() == ALLIANCE ? BG_TEAM_ALLIANCE : BG_TEAM_HORDE)]))
+        {
+            if (carrier->HasAura(BG_WS_SPELL_BRUTAL_ASSAULT))
+                carrier->RemoveAurasDueToSpell(BG_WS_SPELL_BRUTAL_ASSAULT);
+            else if (carrier->HasAura(BG_WS_SPELL_FOCUSED_ASSAULT))
+                carrier->RemoveAurasDueToSpell(BG_WS_SPELL_FOCUSED_ASSAULT);
+        }
+
         Source->CastSpell(Source, SPELL_RECENTLY_DROPPED_FLAG, true);
         UpdateFlagState(Source->GetTeam(), 1);
 
