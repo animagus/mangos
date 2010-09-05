f3aa980ec81e10415d30ce4229316c190c59a90b
SeT <set@wowacadem.ru>
2010-09-05 12:36:20 +0700

    Revert "[patch 57] Not allow to stack same judgement from different casters at target. Patch provided by Laise"
    This reverts commit e5daef4afca4bb011fcb3c467aa2df39806ce54d.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4107,12 +4107,6 @@ bool Unit::AddSpellAuraHolder(SpellAuraHolder *holder)
                     (*itr)->GetTarget()->RemoveSpellAuraHolder((*itr));
                     restart = true;
                     break;
-                }  
-                // Judgements are always single 
-                else if (GetSpellSpecific(holder->GetId()) == SPELL_JUDGEMENT)
-                {
-                    (*itr)->GetTarget()->RemoveSpellAuraHolder((*itr));
-                    break;
                 }
             }
 
