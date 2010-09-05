d2a85c43ec0fea2418fe09397e315ca39ca87015
Insider <insider42rus@yandex.ru>
2010-09-05 14:57:29 +0700

    [patch 66] Implemented server side check for spell global cooldown. Patch provided by darkstalker

--- src/game/Player.h
+++ src/game/Player.h
@@ -154,6 +154,7 @@ struct SpellCooldown
 };
 
 typedef std::map<uint32, SpellCooldown> SpellCooldowns;
+typedef UNORDERED_MAP<uint32 /*category*/, uint32 /*MSTime*/> GlobalCooldowns;
 
 enum TrainerSpellState
 {
@@ -1742,6 +1743,11 @@ class MANGOS_DLL_SPEC Player : public Unit
         void RemoveSpellCategoryCooldown(uint32 cat, bool update = false);
         void SendClearCooldown( uint32 spell_id, Unit* target );
 
+        GlobalCooldowns const& GetGlobalCooldownMap() const { return m_globalCooldowns; }
+        bool HasGlobalCooldown(SpellEntry const* spellInfo) const;
+        uint32 GetGlobalCooldownDelay(SpellEntry const* spellInfo) const;
+        void AddGlobalCooldown(SpellEntry const* spellInfo);
+
         void RemoveArenaSpellCooldowns();
         void RemoveAllSpellCooldown();
         void _LoadSpellCooldowns(QueryResult *result);
@@ -2562,6 +2568,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         PlayerSpellMap m_spells;
         PlayerTalentMap m_talents[MAX_TALENT_SPEC_COUNT];
         SpellCooldowns m_spellCooldowns;
+        GlobalCooldowns m_globalCooldowns;
         uint32 m_lastPotionId;                              // last used health/mana potion in combat, that block next potion use
 
         uint8 m_activeSpec;
