0dcf49767379213c514ec06a76e6ec5e4b5e4f6f
SeT <set@wowacadem.ru>
2010-10-10 02:21:21 +0700

    Revert "Fix gameobject locale"
    This reverts commit b7c71dac43677bdbb80bff88f36002702dfb1b11.

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -516,12 +516,6 @@ struct GameObjectInfo
     }
 };
 
-struct GameObjectLocale
-{
-    std::vector<std::string> Name;
-    std::vector<std::string> CastBarCaption;
-};
-
 // GCC have alternative #pragma pack() syntax and old gcc version not support pack(pop), also any gcc version not support it at some platform
 #if defined( __GNUC__ )
 #pragma pack()
@@ -529,6 +523,11 @@ struct GameObjectLocale
 #pragma pack(pop)
 #endif
 
+struct GameObjectLocale
+{
+    std::vector<std::string> Name;
+    std::vector<std::string> CastBarCaption;
+};
 
 // client side GO show states
 enum GOState
