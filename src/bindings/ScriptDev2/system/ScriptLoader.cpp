38ea7d89e5dbfd0eea87ed2f6d8671a811357abf
SeT <set@wowacadem.ru>
2010-10-30 00:31:38 +0700

    Add lost lines in scripts

--- src/bindings/ScriptDev2/system/ScriptLoader.cpp
+++ src/bindings/ScriptDev2/system/ScriptLoader.cpp
@@ -792,6 +792,12 @@ void AddScripts()
     AddSC_boss_krick();
     AddSC_boss_scourgelord_tyrannus();
 
+    AddSC_instance_halls_of_reflection();
+    AddSC_halls_of_reflection();
+    AddSC_boss_falric();
+    AddSC_boss_marwyn();
+    AddSC_boss_lich_king_hr();
+
     AddSC_boss_anubrekhan();                                //naxxramas
     AddSC_boss_four_horsemen();
     AddSC_boss_faerlina();
@@ -853,7 +859,6 @@ void AddScripts()
     AddSC_instance_pinnacle();
     AddSC_instance_violet_hold();                           //violet_hold
     AddSC_violet_hold();
-
     AddSC_boss_cyanigosa();
     AddSC_boss_erekem();
     AddSC_boss_ichoron();
@@ -861,8 +866,6 @@ void AddScripts()
     AddSC_boss_moragg();
     AddSC_boss_xevozz();
     AddSC_boss_zuramat();
-    AddSC_instance_violet_hold();
-    AddSC_violet_hold();
 
     AddSC_boss_archavon();
     AddSC_boss_emalon();
